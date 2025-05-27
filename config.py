a540fdc604773917ae30c5c808919e844b5e1323
ArsKostromin <arsyakost@gmail.com>
2025-05-27 16:42:11 +0500

    q

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
-from dotenv import load_dotenv
 
-load_dotenv('.env')
 TOKEN = os.environ.get('BOT_TOKEN')
 OWNER = int(os.environ.get('OWNER_ID'))
 DATABASE_USER = os.environ.get('DATABASE_USER')
